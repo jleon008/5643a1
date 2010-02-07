4b268df459f179a4a27a0418bc27876328d015d8
skiingbeast87 <skiingbeast87@003f9772-1412-11df-ad8c-0f209b884837>
2010-02-07 20:44:44 +0000

    hair!

--- 5643a1/cs567/particles/cs567/particles/SpringForceBending.java
+++ 5643a1/cs567/particles/cs567/particles/SpringForceBending.java
@@ -33,9 +33,37 @@ public class SpringForceBending implements Force
     public void applyForce()
     {
 	/// TODO: Accumulate spring and damper(optional) forces into
-	/// p0.f, p1.f, p2.f (Nonzero bend angles are an optional
-	/// feat):
-
+	/// p0.f, p1.f, p2.f (Nonzero bend angles are an optional feat):
+    	
+    	Vector3d a = new Vector3d();
+    	a.sub(p1.x,p0.x);
+    	Vector3d b = new Vector3d();
+    	b.sub(p2.x,p1.x);
+    	
+    	double f2scale = Constants.STIFFNESS_BEND / (2 * b.length());
+    	double f0scale = Constants.STIFFNESS_BEND / (2 * a.length());
+    	
+    	a.normalize();
+    	b.normalize();
+    	
+    	Vector3d f0 = new Vector3d(a);
+    	f0.scale(a.dot(b));
+    	f0.sub(b);
+    	f0.scale(f0scale);
+    	
+    	Vector3d f2 = new Vector3d();
+    	f2.scale(a.dot(b));
+    	f2.sub(a,f2);
+    	f2.scale(f2scale);
+    	
+    	Vector3d f1 = new Vector3d(f0);
+    	f1.negate();
+    	f1.sub(f2);
+    	
+    	p0.f.add(f0);
+    	p1.f.add(f1);
+    	p2.f.add(f2);
+    	
     }
 
     public void display(GL gl)
