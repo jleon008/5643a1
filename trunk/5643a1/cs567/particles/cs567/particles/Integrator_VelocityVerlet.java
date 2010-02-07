28b07baade57c76068548e97e0457d45af7166ba
danewallinga <danewallinga@003f9772-1412-11df-ad8c-0f209b884837>
2010-02-07 21:25:42 +0000

    Integrator stubs

---
+++ trunk/5643a1/cs567/particles/cs567/particles/Integrator_VelocityVerlet.java
@@ -0,0 +1,10 @@
+package cs567.particles;
+
+public class Integrator_VelocityVerlet implements Integrator {
+
+	public void advanceTime(double timestep, DynamicalSystem sys) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
