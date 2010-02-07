3e71d102c2c43079c747dcbb8e36af153af80249
danewallinga <danewallinga@003f9772-1412-11df-ad8c-0f209b884837>
2010-02-07 21:25:42 +0000

    Integrator stubs

---
+++ 5643a1/cs567/particles/cs567/particles/Integrator_Midpoint.java
@@ -0,0 +1,10 @@
+package cs567.particles;
+
+public class Integrator_Midpoint implements Integrator {
+
+	public void advanceTime(double timestep, DynamicalSystem sys) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
